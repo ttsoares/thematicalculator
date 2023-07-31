b97e0de34a6b1a797969953c3fb0d89d57a89ad9
Thomas Soares <ttsoares@orion.ufrgs.br>
2023-07-31 11:37:16 -0300

    Fixed Hydration and default theme issues

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -1,9 +1,24 @@
 "use client";
 
 import { ThemeProvider } from "@wits/next-themes";
+import { useEffect, useState } from "react";
 
 const Providers = ({ children }) => {
-  return <ThemeProvider enableSystem={false}>{children}</ThemeProvider>;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider defaultTheme="theme1" enableSystem={false}>
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
